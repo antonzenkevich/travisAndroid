ddb921e46c81311b850911bf3a4edfd6a781ebb5
ICX\Anton.Ziankevich <anton.ziankevich@itechart-group.com>
2019-04-24 16:35:16 +0300

    android close and open app

--- src/test/java/projectApp/SampleTest.java
+++ src/test/java/projectApp/SampleTest.java
@@ -1,8 +1,14 @@
 package projectApp;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.android.AndroidDriver;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.webdriver.WebDriverFacade;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import projectApp.steps.GeneralPageSteps;
@@ -15,5 +21,20 @@ public abstract class SampleTest {
 
     @Steps
     public GeneralPageSteps generalPageSteps;
+
+    public AppiumDriver returnAppiumDriver() {
+        WebDriverFacade webDriverFacade = (WebDriverFacade) driver;
+        WebDriver webDriver = webDriverFacade.getProxiedDriver();
+
+        return (AppiumDriver) webDriver;
+    }
+
+    @Before
+    public void setup() {
+        returnAppiumDriver().closeApp();
+        System.out.println("close");
+        returnAppiumDriver().launchApp();
+        System.out.println("run");
+    }
 
 }
