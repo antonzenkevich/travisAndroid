a6be16823778374a5360a20cc87b005db5591c79
ICX\Anton.Ziankevich <anton.ziankevich@itechart-group.com>
2019-04-26 17:40:29 +0300

    android close alert

--- src/test/java/projectApp/tests/noteTests/test2.java
+++ src/test/java/projectApp/tests/noteTests/test2.java
@@ -13,8 +13,9 @@ public class test2 extends SampleTest {
     @Test
     public void logInAsClient() {
         if(getDriver().getPageSource().contains("Process system isn't responding")) {
-            getDriver().findElement(MobileBy.xpath("//android.widget.Button[resource-id='android:id/aerr_close']")).click();
-        }        generalPageSteps.loginAsClient("test-android+mgmt-core@perchwell.com","perchwell");
+            getDriver().findElement(MobileBy.id("android:id/aerr_close")).click();
+        }
+        generalPageSteps.loginAsClient("test-android+mgmt-core@perchwell.com","perchwell");
         generalPageSteps.skipAllHints();
         generalPageSteps.clickNotNowButton();
         generalPageSteps.clickOnOpenAccountButton();
