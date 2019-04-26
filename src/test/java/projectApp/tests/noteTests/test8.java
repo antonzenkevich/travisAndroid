0034dc22b79639ad6f1521c15e6cf2c26b91fa92
ICX\Anton.Ziankevich <anton.ziankevich@itechart-group.com>
2019-04-26 19:54:34 +0300

    android 24 560 dpi memory 1536

--- src/test/java/projectApp/tests/noteTests/test8.java
+++ src/test/java/projectApp/tests/noteTests/test8.java
@@ -1,14 +1,26 @@
 package projectApp.tests.noteTests;
 
+import io.appium.java_client.MobileBy;
 import net.thucydides.core.annotations.WithTagValuesOf;
 import org.junit.Test;
 import projectApp.SampleTest;
+
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
 
 @WithTagValuesOf({"TravisTest", "TravisTest1"})
 public class test8 extends SampleTest {
 
     @Test
     public void logInAsClient() {
+        if(getDriver().getPageSource().contains("Process system isn't responding")) {
+            getDriver().findElement(MobileBy.xpath("//android.widget.Button[@resource-id='android:id/aerr_close']")).click();
+        }
         generalPageSteps.loginAsClient("test-android+mgmt-core@perchwell.com","perchwell");
+        generalPageSteps.skipAllHints();
+        generalPageSteps.clickNotNowButton();
+        generalPageSteps.clickOnOpenAccountButton();
+        generalPageSteps.shouldSeeOptionAgents();
+        generalPageSteps.shouldSeeOptionDiscussions();
+        generalPageSteps.shouldSeeOptionTags();
     }
 }
