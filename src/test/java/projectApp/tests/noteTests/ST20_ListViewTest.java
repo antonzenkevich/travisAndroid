6598d8af508f3ab2daa3ce707fbf8796eff645d4
ICX\Anton.Ziankevich <anton.ziankevich@itechart-group.com>
2019-05-02 10:32:17 +0300

    android armeabi 24 pixel with apk install

--- src/test/java/projectApp/tests/noteTests/ST20_ListViewTest.java
+++ src/test/java/projectApp/tests/noteTests/ST20_ListViewTest.java
@@ -1,14 +1,20 @@
 package projectApp.tests.noteTests;
 
+import io.appium.java_client.MobileBy;
 import net.thucydides.core.annotations.WithTagValuesOf;
 import org.junit.Test;
 import projectApp.SampleTest;
+
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
 
 @WithTagValuesOf("SmokeTestSuit")
 public class ST20_ListViewTest extends SampleTest {
 
 	@Test
 	public void listView() {
+		if(getDriver().getPageSource().contains(" isn't responding")) {
+			getDriver().findElement(MobileBy.xpath("//android.widget.Button[@resource-id='android:id/aerr_close']")).click();
+		}
 		generalPageSteps.loginAsClient("test-android+broker-no-listing-mgmt-core@perchwell.com","perchwell");
 		atPerchwellPage.skipAllHints();
 		atPerchPopup.clickNotNowButton();
